508a91afc2fc92e2f5ee87d7a61727a38ffbc6cb
Shubhi Jain <shubhij@microsoft.com>
2025-03-10 14:31:29 +0530

    Validatation changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,3 @@
-terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "staterg"
-    storage_account_name = "shubhijstatesa"
-    container_name       = "tfstate"
-    key                  = "terraform_.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
   location = "East US"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
   description = "Name of the Azure resource group"
   type        = string
-  
+  default = "my-resource-group"
 }
 
 variable "location" {
   description = "Azure location"
   type        = string
-  
+  default = "East US"
 }
 
 variable "storage_account_name" {
   description = "Storage Account Name"
   type        = string
-  
+  default = "mystorageacc123"
 }
